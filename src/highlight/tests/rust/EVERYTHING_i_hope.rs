370571edc3691cb27ecc807373599b26e2259807
Mathspy <mathspy257@gmail.com>
2022-12-06 22:53:04 -0500

    Add trait to Rust rendering test

--- src/highlight/tests/rust/EVERYTHING_i_hope.rs
+++ src/highlight/tests/rust/EVERYTHING_i_hope.rs
@@ -25,6 +25,10 @@ where
 {
 }
 
+pub trait Widget {
+    fn render(self, area: Rect, buf: &mut Buffer);
+}
+
 // This is the main function
 fn main() {
     // Statements here are executed when the compiled binary is called
